6e211d33c33eb2d830c2e2fe49245baed6ac2861
priyanshu-678 <mpriyanshu263@gmail.com>
2021-02-17 19:37:54 +0530

    Revert "Revert "Date: 17-02-2021""
    This reverts commit b694d8eb5da214adada21407e195e6e41ae54e64.

---
+++ The_Minimum_Number_Of_Moves.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <set>
+#include <climits>
+
+using namespace std;
+
+#define mod 998244353
+#define lli long long int
+#define ll long long
+#define li long int
+#define FastIO               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0)
+
+class PNC
+{
+public:
+    vector<unsigned lli> f;
+    lli m;
+    PNC(lli n, lli m = mod)
+    {
+        f.resize(n + 1);
+        this->m = m;
+        f[0] = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            f[i] = (i * f[i - 1]) % m;
+        }
+    }
+    lli power(lli b, lli p, lli m = mod)
+    {
+        lli res = 1;
+        while (p > 0)
+        {
+            if (p & 1)
+                res = ((res % m) * (b % m)) % m;
+            b = ((b % m) * (b % m)) % m;
+            p = p >> 1;
+        }
+        return res;
+    }
+    lli modInverse(lli n, lli m = mod) { return power(n, m - 2, m); }
+    lli factorial(lli n) { return f[n]; }
+    lli nCr(lli n, lli r)
+    {
+        if (r == 0)
+            return 1;
+        return (f[n] * modInverse(f[r], m) % m * modInverse(f[n - r], m) % m) % m;
+    }
+    lli nPr(lli n, lli r)
+    {
+        if (r == 0)
+            return 1;
+        return (f[n] * modInverse(f[n - r], m) % m) % m;
+    }
+};
+
+int main()
+{
+    FastIO;
+    ll tc;
+    cin >> tc;
+    while (tc--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> v(n), diff(n, 0);
+        for (ll i = 0; i < n; i++)
+            cin >> v[i];
+        ll ans = 0;
+        do
+        {
+            auto maxxIt = max_element(v.begin(), v.end());
+            ll maxx = *maxxIt;
+            ll maxxIn = maxxIt - v.begin();
+            for (ll i = 0; i < n; i++)
+                diff[i] = maxx - v[i];
+            ll maxxDiff = *max_element(diff.begin(), diff.end());
+            for (ll i = 0; i < n; i++)
+                if (i != maxxIn)
+                    v[i] += maxxDiff;
+            ans += maxxDiff;
+        } while (*max_element(diff.begin(), diff.end()) != 0);
+        cout << ans << endl;
+    }
+    return 0;
+}
